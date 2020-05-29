9a9598afd97c3fd4e32fca0792c28bc37ccabfdc
Phawit Wongsarawit <phawit.w@obodroid.com>
2020-05-29 13:05:27 +0700

    add create bucket

--- browser/app/js/browser/MainActions.js
+++ browser/app/js/browser/MainActions.js
@@ -28,6 +28,9 @@ export const MainActions = ({
   showMakeBucketModal
 }) => {
   const uploadTooltip = <Tooltip id="tt-upload-file">Upload file</Tooltip>
+  const makeBucketTooltip = (
+    <Tooltip id="tt-create-bucket">Create bucket</Tooltip>
+  )
   const onFileUpload = e => {
     e.preventDefault()
     let files = e.target.files
@@ -64,6 +67,21 @@ export const MainActions = ({
               </label>
             </a>
           </OverlayTrigger>
+          {loggedIn && (
+            <OverlayTrigger placement="left" overlay={makeBucketTooltip}>
+              <a
+                href="#"
+                id="show-make-bucket"
+                className="feba-btn feba-bucket"
+                onClick={e => {
+                  e.preventDefault()
+                  showMakeBucketModal()
+                }}
+              >
+                <i className="far fa-hdd" />
+              </a>
+            </OverlayTrigger>
+          )}
         </Dropdown.Menu>
       </Dropdown>
     )
